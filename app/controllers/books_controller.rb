0ceb1614ca08a185f75a64e93ff81f527516d5b2
1101321 <hcri0909@gmail.com>
2021-10-09 05:46:38 +0000

    [Update] bookの新規登録機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,18 @@
 class BooksController < ApplicationController
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def new
     @book = Book.new
   end
-  
+
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def show
@@ -14,4 +20,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
