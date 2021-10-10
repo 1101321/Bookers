60bec33f17014ae5f74adc802622aa47929745dd
1101321 <hcri0909@gmail.com>
2021-10-10 07:38:38 +0000

    [Add] create.updateが失敗したときのバリデーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
-
-  resources :books
   
-  get '/books' => 'books#new'
+  get '/books' => 'books#index'
+  post '/books' => 'books#create'
+  get '/books' => 'books#new', as: 'new_book'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  get '/books/:id' => 'books#show', as: 'book'
+  patch '/books/:id' => 'books#update'
+  put 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
